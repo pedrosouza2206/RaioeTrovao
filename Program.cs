bb643da32d6455249fb4981ec3116c9c995aef1f
ETEC Adolpho Berezin <e107ti@etec.sp.gov.br>
2024-06-04 22:42:58 -0300

    finalizado

---
+++ Program.cs
@@ -0,0 +1,11 @@
+﻿Console.WriteLine("--- Raio e Trovão ---\n");
+Console.WriteLine("Digite quanto tempo demorou para o raio cair...");
+double segundos =Convert.ToDouble(Console.ReadLine());
+
+Console.WriteLine($"trovão a {segundos} segundos ==> raio caiu a {DistanciaDoRaioKm(segundos):N5} quilômetros");
+
+double DistanciaDoRaioKm(double segundos)
+{
+    const double VelocidadeDoSomKmS = 340.29 / 1000;
+    return segundos * VelocidadeDoSomKmS;
+}
